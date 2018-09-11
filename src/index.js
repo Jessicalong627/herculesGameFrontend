b221b9b00b223679b43a4900dfd232a74fcf3dca
github username <github email address>
2018-09-11 11:19:33 -0400

    cards displaying

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  // Array.prototype.shuffle = function() {
+  //         let i =this.length, j, temp;
+  //         while (--i > 0) {
+  //           j = Math.floor(Math.random() * (i + 1));
+  //           temp = this[j];
+  //           this[j] = this[i];
+  //           this[i] = temp;
+  //         }
+  //       }
+
   let memoryGame = document.getElementById('memory-game')
 
 
@@ -14,8 +25,11 @@ document.addEventListener('DOMContentLoaded', () => {
       cardDiv.dataset.name = `${card.name}`
       cardDiv.innerHTML = `<img class="front-face" src="${card.image_url}">
                            <img class="back-face" src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/ayK0FP2CHrFG8CGDJ1fYgKVa5JP.jpg">`
-      memoryGame.append(cardDiv)}
-
+      memoryGame.append(cardDiv)
+      }
     })
+    let cards = document.getElementsByClassName('memory-card')
+    console.log(cards)
+
   }
 })
