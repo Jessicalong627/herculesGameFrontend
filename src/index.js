682545d7e079b5922df82a8c419cd733a6607e5f
github username <github email address>
2018-09-11 15:47:25 -0400

    A LOT OF THINGS DONE

--- src/index.js
+++ src/index.js
@@ -1,35 +1,71 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-  // Array.prototype.shuffle = function() {
-  //         let i =this.length, j, temp;
-  //         while (--i > 0) {
-  //           j = Math.floor(Math.random() * (i + 1));
-  //           temp = this[j];
-  //           this[j] = this[i];
-  //           this[i] = temp;
-  //         }
-  //       }
-
-  let memoryGame = document.getElementById('memory-game')
+let cardA;
+let cardB;
+let memoryGame = document.getElementById('memory-game')
+let cards;
 
 
-  fetch('http://localhost:3000/api/v1/cards')
-  .then(response => response.json())
-  .then(cardsData => addCardDiv(cardsData))
+fetch('http://localhost:3000/api/v1/cards')
+.then(response => response.json())
+.then(cardsData => addCardDiv(cardsData))
 
-  function addCardDiv(cardsData){
-    cardsData.forEach( card => {
-      for(let i = 0; i < 2; i++){
-      let cardDiv = document.createElement('div')
-      cardDiv.className = "memory-card";
-      cardDiv.dataset.name = `${card.name}`
-      cardDiv.innerHTML = `<img class="front-face" src="${card.image_url}">
-                           <img class="back-face" src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/ayK0FP2CHrFG8CGDJ1fYgKVa5JP.jpg">`
-      memoryGame.append(cardDiv)
-      }
-    })
-    let cards = document.getElementsByClassName('memory-card')
-    console.log(cards)
-
+function addCardDiv(cardsData){
+  cardsData.forEach( card => {
+    for(let i = 0; i < 2; i++){
+    let cardDiv = document.createElement('div')
+    cardDiv.className = "memory-card";
+    cardDiv.dataset.name = `${card.name}`
+    cardDiv.innerHTML = `<img class="front-face" src="${card.image_url}">
+                         <img class="back-face" src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/ayK0FP2CHrFG8CGDJ1fYgKVa5JP.jpg">`
+    memoryGame.append(cardDiv)
+    }
+  })
+  cards = document.querySelectorAll('.memory-card')
+  cards.forEach(card => card.addEventListener('click', flipCard))
   }
-})
+
+  function flipCard() {
+    console.log(this)
+      this.classList.toggle('flip');
+      if (!cardA){
+        cardA = this
+      }else{
+        cardB = this
+      }
+      cardMatch(cardA, cardB);
+  }
+
+
+
+  // console.log(cardA)
+  // console.log(cardB)
+
+  // card.classList.toggle('flip');
+  // if (cardA === undefined){
+  //   cardA = event.target
+  // }else{
+  //   cardB = event.target}
+  //   console.log(cardA)
+  //   console.log(cardB)
+function cardMatch(cardA, cardB) {
+if (cardA.dataset.name === cardB.dataset.name){
+  console.log('GREAT SUCCESS!!')
+  matchSuccess(cardA, cardB);
+  }
+  else{
+    matchFailure(cardA, cardB);
+  }
+}
+
+function matchSuccess(cardA, cardB){
+  cardA.removeEventListener('click', flipCard)
+  cardB.removeEventListener('click', flipCard)
+  cardA = null
+  cardB = null
+  console.log(cardA)
+  console.log(cardB)
+  cardMatch();
+}
+
+function matchFailure(cardA, cardB){
+
+}
