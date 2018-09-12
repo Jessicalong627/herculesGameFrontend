0a402e46a20df57c2b9eb96f768e306ffa3c70f9
github username <github email address>
2018-09-12 09:32:54 -0400

    changed before we ruin everything

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ let cardA;
 let cardB;
 let memoryGame = document.getElementById('memory-game')
 let cards;
+let attempts =document.getElementById('attempts')
+
 
 
 fetch('http://localhost:3000/api/v1/cards')
@@ -14,58 +16,55 @@ function addCardDiv(cardsData){
     let cardDiv = document.createElement('div')
     cardDiv.className = "memory-card";
     cardDiv.dataset.name = `${card.name}`
+    cardDiv.dataset.match = 'no-match'
     cardDiv.innerHTML = `<img class="front-face" src="${card.image_url}">
                          <img class="back-face" src="https://image.tmdb.org/t/p/w300_and_h450_bestv2/ayK0FP2CHrFG8CGDJ1fYgKVa5JP.jpg">`
     memoryGame.append(cardDiv)
     }
+    addEventListener()
   })
-  cards = document.querySelectorAll('.memory-card')
-  cards.forEach(card => card.addEventListener('click', flipCard))
+  }
+  function addEventListener(){
+    cards = document.querySelectorAll("[data-match='no-match']")
+    cards.forEach(card => card.addEventListener('click', flipCard))
   }
 
   function flipCard() {
-    console.log(this)
-      this.classList.toggle('flip');
-      if (!cardA){
-        cardA = this
-      }else{
-        cardB = this
-      }
-      cardMatch(cardA, cardB);
+    if (!cardA){
+
+      cardA = this
+      this.classList.toggle('flip')
+
+    }else{
+
+      cardB = this
+        this.classList.toggle('flip');
+    }
+    cardMatch()
   }
 
 
-
-  // console.log(cardA)
-  // console.log(cardB)
-
-  // card.classList.toggle('flip');
-  // if (cardA === undefined){
-  //   cardA = event.target
-  // }else{
-  //   cardB = event.target}
-  //   console.log(cardA)
-  //   console.log(cardB)
-function cardMatch(cardA, cardB) {
-if (cardA.dataset.name === cardB.dataset.name){
-  console.log('GREAT SUCCESS!!')
-  matchSuccess(cardA, cardB);
+  function cardMatch() {
+    let match = cardA.dataset.name === cardB.dataset.name
+     match ? matchSuccess() :  matchFailure()
   }
-  else{
-    matchFailure(cardA, cardB);
+
+  function matchSuccess(){
+    cardA.removeEventListener('click', flipCard)
+    cardB.removeEventListener('click', flipCard)
+    cardA = null
+    cardB = null
   }
-}
 
-function matchSuccess(cardA, cardB){
-  cardA.removeEventListener('click', flipCard)
-  cardB.removeEventListener('click', flipCard)
-  cardA = null
-  cardB = null
-  console.log(cardA)
-  console.log(cardB)
-  cardMatch();
-}
-
-function matchFailure(cardA, cardB){
-
+function matchFailure(){
+  setTimeout(() => {
+    cardA.classList.remove('flip');
+    cardB.classList.remove('flip');
+    cardA = null
+    cardB = null
+    attempts.innerText -= 1
+    if (attempts.innerText == 0) {
+      alert('Game over! Try again!')
+    }
+  }, 750);
 }
